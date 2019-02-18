a2ab228e5b76290838858a3869c8f46a2ac314d0
盧志昇 <billy@luzhishengde-MacBook-Pro.local>
2019-02-18 20:58:16 +0800

    update

--- app/src/main/java/com/billy/animationmenudemo/AnimationMenu.kt
+++ app/src/main/java/com/billy/animationmenudemo/AnimationMenu.kt
@@ -2,19 +2,41 @@ package com.billy.animationmenudemo
 
 import android.animation.ObjectAnimator
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Rect
 import android.support.design.widget.FloatingActionButton
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import kotlinx.android.synthetic.main.animation_menu.view.*
 
 class AnimationMenu: FrameLayout {
     private val TAG = javaClass.simpleName
 
+    private val DEFAULT_BUTTON_SIZE = 56
+    private val DEFAULT_DISTANCE = DEFAULT_BUTTON_SIZE * 1.5f
+    private val DEFAULT_RING_SCALE_RATIO = 1.3f
+    private val DEFAULT_CLOSE_ICON_ALPHA = 0.3f
+
     private val END_POSITION = 240
     private val DURATION: Long = 200
     private var isClicked = false
+
+    private val mButtons = java.util.ArrayList<View>()
+    private val mButtonRect = Rect()
+
+    private var mIconMenu: Int = 0
+    private var mIconClose: Int = 0
+    private var mDurationRing: Int = 0
+    private var mLongClickDurationRing: Int = 0
+    private var mDurationOpen: Int = 0
+    private var mDurationClose: Int = 0
+    private var mDesiredSize: Int = 0
+    private var mRingRadius: Int = 0
+    private var mDistance: Float = 0.toFloat()
+
 
     constructor(context: Context): this(context, null) {
         initLayout(context)
@@ -54,9 +76,49 @@ class AnimationMenu: FrameLayout {
             iconsIds.recycle()
         }
 
+        mDurationRing = typedArray.getInteger(
+            R.styleable.AnimationMenu_duration_ring,
+            resources.getInteger(android.R.integer.config_mediumAnimTime)
+        )
+        mLongClickDurationRing = typedArray.getInteger(
+            R.styleable.AnimationMenu_long_click_duration_ring,
+            resources.getInteger(android.R.integer.config_longAnimTime)
+        )
+        mDurationOpen = typedArray.getInteger(
+            R.styleable.AnimationMenu_duration_open,
+            resources.getInteger(android.R.integer.config_mediumAnimTime)
+        )
+        mDurationClose = typedArray.getInteger(
+            R.styleable.AnimationMenu_duration_close,
+            resources.getInteger(android.R.integer.config_mediumAnimTime))
+
+        val density = context.getResources().getDisplayMetrics().density
+        val defaultDistance = DEFAULT_DISTANCE * density;
+        mDistance = typedArray.getDimension(R.styleable.AnimationMenu_distance, defaultDistance);
+
 
         initLayout(context)
         initMenu()
+        initButtons(context, icons, colors)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val w = resolveSizeAndState(mDesiredSize, widthMeasureSpec, 0)
+        val h = resolveSizeAndState(mDesiredSize, heightMeasureSpec, 0)
+
+        setMeasuredDimension(w, h)
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        ringView.setStrokeWidth(mButtonRect.width())
+        ringView.setRadius(mRingRadius)
+
+        val lp = ringView.layoutParams as FrameLayout.LayoutParams
+        lp.width = right - left
+        lp.height = bottom - top
     }
 
 
@@ -65,6 +127,12 @@ class AnimationMenu: FrameLayout {
         setWillNotDraw(true)
         clipChildren = false
         clipToPadding = false
+
+        val density = context.resources.displayMetrics.density
+        val buttonSize = DEFAULT_BUTTON_SIZE * density
+
+        mRingRadius = (buttonSize + (mDistance - buttonSize / 2)).toInt()
+        mDesiredSize = (mRingRadius * 2 * DEFAULT_RING_SCALE_RATIO).toInt()
     }
 
     private fun initMenu() {
@@ -79,6 +147,14 @@ class AnimationMenu: FrameLayout {
                 //menuListener.menuExpand()
                 Log.i(TAG, "${menu_fab.x}, ${menu_fab.y - END_POSITION}")
                 //initButtons(context, 6, menu_fab.x, menu_fab.y)
+                Log.i(TAG, "x: ${menu_fab.x}, y:${menu_fab.y}")
+
+                val buttonNumber = mButtons.indexOf(menu_fab) + 1
+                val stepAngle = 360f / mButtons.size
+                val rOStartAngle = 270 - stepAngle + stepAngle * buttonNumber
+                val rStartAngle = if (rOStartAngle > 360) rOStartAngle % 360 else rOStartAngle
+
+                ringView.setStartAngle(rStartAngle)
             } else {
                 var y = menu_fab.translationY
                 var animator = ObjectAnimator.ofFloat(menu_fab, "translationY", y, y + END_POSITION)
@@ -89,9 +165,27 @@ class AnimationMenu: FrameLayout {
                 //menuListener.menuClosed()
             }
         }
+
+        ringView.x = menu_fab.x
+        ringView.y = menu_fab.y - END_POSITION
     }
 
     private fun initButtons(context: Context, icons: List<Int>, colors: List<Int>) {
+        val buttonsCount = Math.min(icons.size, colors.size)
+        for (i in 0 until buttonsCount) {
+            val button = FloatingActionButton(context)
+            button.setImageResource(icons[i])
+            button.backgroundTintList = ColorStateList.valueOf(colors[i])
+            button.isClickable = true
+//            button.setOnClickListener(OnButtonClickListener())
+//            button.setOnLongClickListener(OnButtonLongClickListener())
+            button.scaleX = 0f
+            button.scaleY = 0f
+            button.layoutParams =
+                FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT)
 
+            addView(button)
+            mButtons.add(button)
+        }
     }
 }
--- app/src/main/java/com/billy/animationmenudemo/RingView.kt
+++ app/src/main/java/com/billy/animationmenudemo/RingView.kt
@@ -13,10 +13,13 @@ class RingView: View {
     private val mPaint: Paint
     private val mPath = Path()
 
+    private var mAngle: Float = 0.toFloat()
+    private var mStartAngle: Float = 0.toFloat()
+    private var mRadius: Int = 0
+
 
     constructor(context: Context): this(context, null)
     constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
-
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr)
 
     init {
@@ -31,13 +34,48 @@ class RingView: View {
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+        if (!mPath.isEmpty) {
+            canvas!!.save()
+            canvas.translate((width / 2).toFloat(), (height / 2).toFloat())
+            canvas.drawPath(mPath, mPaint)
+            canvas.restore()
+        }
+    }
 
-        var rectF = RectF(100f, 200f, 300f, 400f)
-        canvas!!.drawArc(rectF, 0f, 360f, true, mPaint)
+    override fun setAlpha(alpha: Float) {
+        super.setAlpha(alpha)
 
+        mPaint.alpha = (255 * alpha).toInt()
+        invalidate()
+    }
 
-
+    override fun getAlpha(): Float {
+        return (mPaint.alpha / 255).toFloat()
 
     }
 
+    fun setStartAngle(startAngle: Float) {
+        mStartAngle = startAngle
+        mAngle = 0f
+
+        val sw = mPaint.strokeWidth * 0.5f
+        val radius = mRadius - sw
+
+        mPath.reset()
+        val x = Math.cos(Math.toRadians(startAngle.toDouble())).toFloat() * radius
+        val y = Math.sin(Math.toRadians(startAngle.toDouble())).toFloat() * radius
+        mPath.moveTo(x, y)
+    }
+
+    fun setStrokeColor(color: Int) {
+        mPaint.color = color
+    }
+
+    fun setStrokeWidth(width: Int) {
+        mPaint.strokeWidth = width.toFloat()
+    }
+
+    fun setRadius(radius: Int) {
+        mRadius = radius
+    }
 }
