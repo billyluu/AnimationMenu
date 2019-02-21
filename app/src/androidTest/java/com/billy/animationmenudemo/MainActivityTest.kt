2491ced63ab69c5e472a225128db9837d4f63f45
sheng0208 <cobi90190@gmail.com>
2019-02-21 11:17:49 +0800

    update

---
+++ app/src/androidTest/java/com/billy/animationmenudemo/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.billy.animationmenudemo
+
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onCreate() {
+
+    }
+}
