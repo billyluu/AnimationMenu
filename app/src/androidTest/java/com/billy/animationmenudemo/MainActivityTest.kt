8a2f6a85bb159c640a8d0c5aee09e9016c0f6954
sheng0208 <cobi90190@gmail.com>
2019-02-21 14:54:58 +0800

    add AnimationMenu UI TEST

--- app/src/androidTest/java/com/billy/animationmenudemo/MainActivityTest.kt
+++ app/src/androidTest/java/com/billy/animationmenudemo/MainActivityTest.kt
@@ -1,25 +1,85 @@
 package com.billy.animationmenudemo
 
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import org.junit.After
-import org.junit.Before
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.view.View
+import android.view.ViewGroup
+import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
+@LargeTest
+@RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @Before
-    fun setUp() {
-    }
-
-    @After
-    fun tearDown() {
-    }
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun onCreate() {
+    fun mainActivityTest() {
+        val floatingActionButton = onView(
+            allOf(
+                withId(R.id.menu_fab),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.animation_menu),
+                        childAtPosition(
+                            withClassName(`is`("android.support.constraint.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton.perform(click())
 
+        val floatingActionButton2 = onView(
+            allOf(
+                withId(R.id.menu_fab),
+                childAtPosition(
+                    allOf(
+                        withId(R.id.animation_menu),
+                        childAtPosition(
+                            withClassName(`is`("android.support.constraint.ConstraintLayout")),
+                            1
+                        )
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        floatingActionButton2.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
     }
 }
